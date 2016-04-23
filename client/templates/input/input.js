bd3dc053105783d0fafbfbf3e20dfd10eb00ce56
javiergarciagonzalez <javier.garciagonzalez@bq.com>
2016-04-23 12:01:49 +0200

    Add user accounts and login feature

--- client/templates/input/input.js
+++ client/templates/input/input.js
@@ -2,7 +2,7 @@ Template.input.events = {
 	'keydown input#message': function (event) {
 		if(event.which == 13) { // 13 is the enter key event
 			if(Meteor.user())
-				var name = Meteor.user().profile.name;
+				var name = Meteor.user().username;
 			else
 				var name = 'Anonymous';
 			var message = document.getElementById('message');
